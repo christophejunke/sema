50d2ace946d2ff255b5fd6bcf6c6f472e922a7d2
Christophe Junke <junke.christophe@gmail.com>
2018-04-04 20:56:30 +0200

    More tasks

--- sem.lisp
+++ sem.lisp
@@ -11,20 +11,21 @@
 
 (defmacro with-info ((context) &body forms)
   (once-only (context)
-    `(macrolet ((info (tag &optional form)
+    `(macrolet ((info (tag &optional (form (list 'quote tag)))
                   `(progn (emit ,tag ,',context)
                           ,form)))
        ,@forms)))
 
 (defmacro info (&rest args)
   (declare (ignore args))
-  (error "Not inside with-info."))
+  (error "Not inside WITH-INFO."))
 
 (defun controller (nb-threads *standard-output* check-point wake-up)
   (with-info (:controller)
     (info :synchronize (wait-on-semaphore check-point :n nb-threads))
     (info :wake        (signal-semaphore wake-up nb-threads))
-    (info :done)))
+    (info :synchronize (wait-on-semaphore check-point :n nb-threads))
+    (info :wake        (signal-semaphore wake-up nb-threads))))
 
 (defun task (time)
   (lambda (*standard-output* check-point wake-up)
@@ -32,24 +33,27 @@
       (info `(:sleep ,time)     (sleep time))
       (info :reached-checkpoint (signal-semaphore check-point))
       (info :synchronize        (wait-on-semaphore wake-up))
+      (info `(:sleep ,time)     (sleep time))
+      (info :reached-checkpoint (signal-semaphore check-point))
+      (info :synchronize        (wait-on-semaphore wake-up))
       (info :done))))
 
-(defun main ()
-  (let ((arguments (list *standard-output*
-                         (make-semaphore :name "check-point")
-                         (make-semaphore :name "wake-up"))))
-    (flet ((thread (name function)
-             (make-thread function :name name :arguments arguments)))
-      (let ((threads
-              (list (thread "sema/task-1" (task (float 3/100)))
-                    (thread "sema/task-2" (task (float 2/100))))))
-        (map ()
-             #'join-thread
-             (list* (make-thread #'controller
-                                 :name "seam/control"
-                                 :arguments (list* (length threads) arguments))
-                    threads))))))
+(defun make-threads (arguments)
+  (loop for i below 10
+        collect (make-thread (task (+ 0.01d0 (random 1.0d0)))
+                             :name (format nil "sema/task-~a" i)
+                             :arguments arguments)))
 
+(defun main ()
+  (let* ((arguments (list *standard-output*
+                          (make-semaphore :name "check-point")
+                          (make-semaphore :name "wake-up")))
+         (threads (make-threads arguments)))
+    (mapc #'join-thread (list* (make-thread #'controller
+                                            :name "seam/control"
+                                            :arguments (list* (length threads)
+                                                              arguments))
+                               threads))))
 
 (with-open-file (*standard-output* #P"/home/chris/src/sema/lisp.out"
                                    :direction :output
